a5c7cc8fcaa1f14933f67b1efa1513bbdd9913c6
bmccarson <brian@mccarson.us>
2025-09-04 22:23:36 -0400

    create cache

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,66 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type PokeCache struct {
+	cache    map[string]cacheEntry
+	interval time.Duration
+	mu       sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func New(interval time.Duration) *PokeCache {
+	cache := PokeCache{
+		cache:    make(map[string]cacheEntry),
+		interval: interval,
+		mu:       sync.Mutex{},
+	}
+
+	go cache.reapLoop()
+
+	return &cache
+}
+
+func (pc *PokeCache) Add(key string, val []byte) {
+	pc.mu.Lock()
+	defer pc.mu.Unlock()
+
+	newEntry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+
+	pc.cache[key] = newEntry
+}
+
+func (pc *PokeCache) Get(key string) ([]byte, bool) {
+	pc.mu.Lock()
+	defer pc.mu.Unlock()
+
+	if cache, exists := pc.cache[key]; exists {
+		return cache.val, true
+	}
+
+	return nil, false
+}
+
+func (pc *PokeCache) reapLoop() {
+	for {
+		time.Sleep(pc.interval)
+
+		for key, entry := range pc.cache {
+			if entry.createdAt.After(time.Now().Add(-pc.interval)) {
+				pc.mu.Lock()
+				delete(pc.cache, key)
+				pc.mu.Unlock()
+			}
+		}
+	}
+}
